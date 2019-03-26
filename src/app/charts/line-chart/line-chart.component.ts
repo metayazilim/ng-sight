df62c0495f0d54e08ef3c371a2e8c7a505f041c4
Mehmet Tanrıverdi <mehmet422@gmail.com>
2019-03-26 12:03:10 +0300

    Created sample data

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  {data: [32, 24, 24, 36, 32, 28], label: 'Gübre Separatörü'},
+  {data: [30, 28, 13, 18, 41, 16], label: 'Dalgıç Pompa'},
+  {data: [25, 15, 24, 20, 22, 12], label: 'Dalgıç Karıştırıcı'}
+];
+
+const LINE_CHART_LABELS: string[] = ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran'];
 
 @Component({
   selector: 'app-line-chart',
@@ -8,7 +16,15 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
-
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+  lineChartLegend = true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;
   ngOnInit() {
   }
 
